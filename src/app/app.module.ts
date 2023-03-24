9cba834a11349aa6e8c72a247c9693cc6a0a53b3
PdskDev <prestadesk@gmail.com>
2023-03-24 22:18:23 +0100

    NgRx - init - Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { ContactDialogComponent } from './contact-dialog/contact-dialog.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactFormPageComponent } from './contact-form-page/contact-form-page.component';
+import { ContactService } from './contact.service';
+import { CustomFormsModule } from 'ng2-validation';
+import { FormsModule } from '@angular/forms';
 import { HomePageComponent } from './home-page/home-page.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 import { SearchContactsPipe } from './search-contacts.pipe';
+import { StoreModule } from '@ngrx/store';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { reducers } from './reducers';
 
 @NgModule({
   declarations: [
@@ -19,14 +37,32 @@ import { SearchContactsPipe } from './search-contacts.pipe';
     ContactFormPageComponent,
     HomePageComponent,
     TopBarComponent,
-    SearchContactsPipe
+    SearchContactsPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatCardModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CustomFormsModule,
+
+    StoreModule.forRoot(reducers),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ContactService, SearchContactsPipe],
+  bootstrap: [AppComponent],
+  entryComponents: [ContactDialogComponent],
 })
-export class AppModule { }
+export class AppModule {}
