5db0857f1e528c30169076c0bdd076fc6619c3be
PdskDev <prestadesk@gmail.com>
2023-03-24 21:40:35 +0100

    Components + Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { ContactDialogComponent } from './contact-dialog/contact-dialog.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactFormPageComponent } from './contact-form-page/contact-form-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SearchContactsPipe } from './search-contacts.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactDialogComponent,
+    ContactFormComponent,
+    ContactFormPageComponent,
+    HomePageComponent,
+    TopBarComponent,
+    SearchContactsPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
